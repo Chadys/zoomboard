d5e9a1c88720c130be16dea6e3adfbac3806a3a5
chadys <chadys@hotmail.fr>
2018-02-26 14:41:33 +0100

    result saving

--- sentences_manager.js
+++ sentences_manager.js
@@ -1,50 +1,14 @@
-(function() {
-    window.corpus = [
-        "quelle belle journee",
-        "le ciel est bleu",
-        "la terre est plate",
-        "si je rempli mon verre il sera rempli",
-        "je crois que le belvedere est de travers",
-        "le rugby est un sport d equipe",
-        "attention a la marche en descendant du train",
-        "il est indubitablement grand",
-        "justifiez votre acte",
-        "trouver le chemin vers le village",
-        "dormez paisiblement en cette belle nuit",
-        "obtemperez ou mourrez",
-        "la glace a la vanille est le meilleur choix",
-        "publiez cet article sur le journal",
-        "le destin de cet insecte est tout trace",
-        "voulez vous dormir sur le canape",
-        "melanger le sucre et la farine",
-        "quelle delicieuse journee",
-        "la poule est un animal diurne",
-        "sans desespoir il n'y a pas d espoir",
-        "les humains sont nes dans le feu",
-        "produire du lait de maniere massive",
-        "l enfer c est les autres",
-        "j aime les compotes de pommes",
-        "votre point de vue est legitime",
-        "detruire et creer sont des principes lies",
-        "ce n est qu une question de temps",
-        "vous avez une superbe memoire",
-        "mangerez vous du choux",
-        "cinq cents ans apres il garde son eclat",
-        "je suis subjugue par cette paire de chaussures",
-        "klaxonner dans un embouteillage est inutile",
-        "former les bataillons",
-        "exercer un grand ascendant sur quelqu un",
-        "qualifie un courant d air qui se dirige vers le haut",
-        "un astre au dessus de l horizon"
-    ];
-    window.LetterColorEnum = Object.freeze({NEUTRAL: "#0b559b", CORRECT: "#0B610B", ERROR: "#B40404"});
-}());
 (function () {
+    var LetterColorEnum = Object.freeze({NEUTRAL: "#0b559b", CORRECT: "#0B610B", ERROR: "#B40404"});
     $.widget("manager.sentences_manager", {
         options: {
 
         },
         _create: function() {
+            this.keyStroke = 0;
+            this.nCharacters = 0;
+            this.nWords = 0;
+            this.typedText = "";
             this.addNewSentence();
             this._refresh();
         },
@@ -56,7 +20,8 @@
             }
         },
         addNewSentence: function () {
-            this.letterIndex = 0;
+            // this.letterIndex = 0;
+            this.typedText = "";
             this.sentence = [];
             var selectedSentence = corpus[Math.floor(Math.random() * Math.floor(corpus.length))];
             for(var i = 0; i<selectedSentence.length; i++) {
@@ -78,25 +43,42 @@
             }
         },
         removeLetter: function () {
-            if (this.letterIndex === 0) return;
-            this.letterIndex--;
-            this.sentence[this.letterIndex].color = LetterColorEnum.NEUTRAL;
-            this._refresh();
+            // if (this.letterIndex === 0) return;
+            // this.letterIndex--;
+            // this.sentence[this.letterIndex].color = LetterColorEnum.NEUTRAL;
+            // this._refresh();
+            this.typedText.slice(0, -1);
+            this.keyStroke++;
         },
         endSentence: function () {
+            this.updateStat();
             var endSentenceEvent = jQuery.Event("sentence_finished");
             this.element.trigger(endSentenceEvent);
             this.addNewSentence();
             this._refresh();
+            //TODO check distance de levenshtein pour MSD
+        },
+        updateStat: function () {
+            this.nCharacters += this.typedText.length;
+            this.nWords += this.typedText.split(" ").length;
         },
         addLetter: function (letter) {
-            if (this.letterIndex === this.sentence.length){
-                return;
-            }
-            this.sentence[this.letterIndex].color = (letter === this.sentence[this.letterIndex].letter) ?
-                LetterColorEnum.CORRECT : LetterColorEnum.ERROR;
-            this.letterIndex++;
-            this._refresh();
+            // if (this.letterIndex === this.sentence.length){
+            //     return;
+            // }
+            // this.sentence[this.letterIndex].color = (letter === this.sentence[this.letterIndex].letter) ?
+            //     LetterColorEnum.CORRECT : LetterColorEnum.ERROR;
+            // this.letterIndex++;
+            // this._refresh();
+            this.typedText += letter;
+            this.keyStroke++;
+        },
+        endTest: function (time) {
+            this.updateStat();
+            var nMinute = time/1000/60;
+            var kspc = this.nCharacters === 0 ? 0 : this.keyStroke / this.nCharacters;
+            var wpm = this.nWords / nMinute;
+            window.location.href = "http://localhost:8000/save_result?kspc="+kspc+"&wpm="+wpm;
         }
     });
 }());
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 var express = require('express');
+var fs = require('fs');
+var url = 'http://localhost:8000/';
 
 var app = express.createServer();
 app.configure(function() {
@@ -8,5 +10,21 @@ app.configure(function() {
 	app.use(express.static(__dirname));
 });
 
+app.get('/thanks', function(req, res){
+    res.sendfile('thanks.html', {root: __dirname});
+});
+
+app.get('/save_result', function(req, res){
+    var textResult = "kspc : " + req.query["kspc"] + " ; wpm : " + req.query["wpm"];
+    fs.writeFileSync('./result.txt', textResult, function (err) {
+        if (err) {
+            return console.log('there is an error');
+        }
+
+        console.log('the file was saved');
+    });
+    res.redirect(url + 'thanks');
+});
+
 app.listen(8000);
-console.log("Fast times at http://localhost:8000/");
+console.log("Fast times at " + url);
